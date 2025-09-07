653a42d06daae68779e826cf57843fce3b78ce38
Amankwah Foster <multiwebgh@gmail.com>
2025-09-07 12:34:14 +0000

    Added temp new

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
-    path("books/", views.list_books, name="list_books"),   # existing
-    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),  # existing
+    path("books/", views.list_books, name="list_books"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 
-    # New authentication routes
+    
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+
     path("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
 ]
