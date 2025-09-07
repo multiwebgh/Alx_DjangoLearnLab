8818daa1939aad66ae0f6293b55606bf96207d4f
Amankwah Foster <multiwebgh@gmail.com>
2025-09-07 12:09:40 +0000

    html new

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -25,12 +25,12 @@ def get_librarian_for_library(library_name):
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
 
 # Class-based view to show library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
-
 
