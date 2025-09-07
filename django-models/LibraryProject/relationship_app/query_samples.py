c5769076505f537a9dbba47a89dfe4973815c3d9
Amankwah Foster <multiwebgh@gmail.com>
2025-09-07 10:25:26 +0000

    Fixed queries to match ALX checker requirements

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,10 +1,10 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-books_by_author = Book.objects.filter(author__name="Author Name")
+books_by_author = Book.objects.filter(author__name=author_name)
 
 # List all books in a library
-books_in_library = Library.objects.get(name="Library Name").books.all()
+books_in_library = Library.objects.get(name=library_name).books.all()
 
 # Retrieve the librarian for a library
-librarian_for_library = Library.objects.get(name="Library Name").librarian
+librarian_for_library = Library.objects.get(name=library_name).librarian
