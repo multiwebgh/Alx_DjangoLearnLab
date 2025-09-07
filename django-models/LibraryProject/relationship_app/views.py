0430fe87efba1da516a45126b4f0d522ee2a7cf1
Amankwah Foster <multiwebgh@gmail.com>
2025-09-07 11:59:23 +0000

    html add

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,20 +3,6 @@ from .models import Book, Library
 from django.views.generic import DetailView
 
 
-
-
-# Function-based view to list all books
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
-
-# Class-based view to show library details
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = "library_detail.html"
-    context_object_name = "library"
-
-
 # Create your views here.
 from relationship_app.models import Author, Book, Library, Librarian
 
@@ -33,5 +19,18 @@ def get_books_in_library(library_name):
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
     return library.librarian
+
+
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "list_books.html", {"books": books})
+
+# Class-based view to show library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
 
 
