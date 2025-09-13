d86c3cbd7c049b27b78df50e817256ac1a5417e3
Amankwah Foster <multiwebgh@gmail.com>
2025-09-13 11:55:41 +0000

    another file2

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('view_books/', views.view_books, name='view_books'),
+    path('book_list/', views.book_list, name='book_list'),
     path('create_book/', views.create_book, name='create_book'),
     path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
     path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 
 @permission_required('bookshelf.can_view', raise_exception=True)
-def view_books(request):
+def book_list(request):   # <-- use book_list instead of view_books
     books = Book.objects.all()
-    return render(request, 'bookshelf/view_books.html', {'books': books})
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
