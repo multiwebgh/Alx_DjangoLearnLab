f693fafa2462edd65235b5c066c5e3022766b00a
Amankwah Foster <multiwebgh@gmail.com>
2025-09-20 13:59:14 +0000

    update is added

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
+from rest_framework import viewsets
+
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
