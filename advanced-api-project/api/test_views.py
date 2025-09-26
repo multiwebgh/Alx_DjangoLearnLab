86af1dd891b6d64ff0f2567e72ff2b75762b9e6f
Amankwah Foster <multiwebgh@gmail.com>
2025-09-26 20:59:08 +0000

    Initial commit: Advanced new234

---
+++ advanced-api-project/api/test_views.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.contrib.auth.models import User
+from .models import Book
+
+
