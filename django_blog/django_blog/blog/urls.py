b30a624d38cbbe5c006f2c036c2fab8a5092fe0a
multiwebgh <multiwebgh@gmail.com>
2025-10-04 15:45:48 +0000

    Delete django_blog/django_blog directory
    deleted file

--- django_blog/django_blog/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from blog import views
-
-urlpatterns = [
-     path('', views.home, name='home' ),
-    #  path('', views.posts, name='home' ),
-    #  path('', views.login, name='home' ),
-    #  path('', views.register, name='home' ),
-]
