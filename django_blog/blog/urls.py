3b978b8ef52f7d7439d009d0de89610ce6ed21ae
Amankwah Foster <multiwebgh@gmail.com>
2025-10-04 18:52:51 +0000

    an updatde 10

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,20 +1,10 @@
 from django.urls import path
-from blog import views
+from . import views
 
 urlpatterns = [
-    path('', views.home, name='home' ),
-    path('posts/', views.posts, name='posts'),
-    path('register/', views.register, name='register'),
-    path('login/', views.UserLoginView.as_view(), name='login'),
-    path('logout/', views.UserLogoutView.as_view(), name='logout'),
-    path('profile/', views.profile, name='profile'),
-    path('posts/', views.PostListView.as_view(), name='posts'),
-    # create new post
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-    # detail
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    # edit
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
-    # delete
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('', views.PostListView.as_view(), name='posts'),  # list view (homepage for posts)
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),  # create new post
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),  # single post view
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),  # update post
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),  # delete post
 ]
