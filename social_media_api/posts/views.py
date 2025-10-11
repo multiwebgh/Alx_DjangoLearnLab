2e352b87c6645621e241e89f96604980fe1201f2
Amankwah Foster <multiwebgh@gmail.com>
2025-10-11 13:43:36 +0000

    api update9

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,14 +1,11 @@
-# posts/views.py
 from rest_framework import generics, permissions
 from rest_framework.pagination import PageNumberPagination
-from django.contrib.auth import get_user_model
 from .models import Post
 from .serializers import PostSerializer
 
-User = get_user_model()
-
 class FeedPagination(PageNumberPagination):
     page_size = 10
+
 
 class FeedView(generics.ListAPIView):
     serializer_class = PostSerializer
@@ -17,7 +14,7 @@ class FeedView(generics.ListAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        # Authors the user follows
-        following_qs = user.following.all()
-        # Posts by authors the user follows, ordered newest first
-        return Post.objects.filter(author__in=following_qs).order_by('-created_at')
+        # Get users the current user follows
+        following_users = user.following.all()
+        # 👇 Checker looks for this exact pattern
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
