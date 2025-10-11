c6a7cb69c623c9a497857066c68da26d2a96c4ff
Amankwah Foster <multiwebgh@gmail.com>
2025-10-11 13:19:00 +0000

    api update6

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,40 +1,32 @@
-from rest_framework import viewsets, filters, mixins
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from django_filters.rest_framework import DjangoFilterBackend
-from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from .permissions import IsOwnerOrReadOnly
 
+# Permission to allow only owners to edit/delete
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Read-only permissions for safe methods
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions only for the owner
+        return obj.author == request.user
+
+
+# ViewSet for Posts
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.select_related('author').prefetch_related('comments').all()
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['author__username']   # allow filtering by author username
-    search_fields = ['title', 'content']
-    ordering_fields = ['created_at', 'updated_at']
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
+# ViewSet for Comments
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.select_related('author', 'post').all()
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['post', 'author__username']
-    search_fields = ['content']
-    ordering_fields = ['created_at', 'updated_at']
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-    # Optional: restrict list to comments for a specific post if provided via query param
-    def get_queryset(self):
-        qs = super().get_queryset()
-        post_id = self.request.query_params.get('post')
-        if post_id:
-            qs = qs.filter(post__id=post_id)
-        return qs
